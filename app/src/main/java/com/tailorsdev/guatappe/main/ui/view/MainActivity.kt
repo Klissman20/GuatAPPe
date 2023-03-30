62b0ca2b9180cd57c66d961ea11e2429477bb6a6
Klissman20 <klesteban0.3@gmail.com>
2023-03-30 17:39:56 -0500

    ARCore dependencies & implementations

--- app/src/main/java/com/tailorsdev/guatappe/main/ui/view/MainActivity.kt
+++ app/src/main/java/com/tailorsdev/guatappe/main/ui/view/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
