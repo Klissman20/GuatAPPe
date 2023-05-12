6cce12202dfc58414c16b4471d831aa192d23734
David Roman <roman.david@gmail.com>
2023-05-12 17:33:13 -0500

    add fonts Roboto Regular and Bold

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -6,5 +6,5 @@ class AppTheme {
   static Color colorAppAlpha = const Color.fromARGB(0x99, 0xDB, 0x41, 0x1F);
 
   ThemeData getTheme() =>
-      ThemeData(useMaterial3: true, colorSchemeSeed: colorApp);
+      ThemeData(useMaterial3: true, colorSchemeSeed: colorApp, fontFamily: 'Roboto');
 }
